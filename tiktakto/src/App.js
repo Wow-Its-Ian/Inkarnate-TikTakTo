ca496b86de95f43b44d9ec7693bb7d81c6e67cfe
Wow-Its-Ian <wallsianh@gmail.com>
2021-09-14 16:08:20 -0400

    Game start button functionality complete

--- tiktakto/src/App.js
+++ tiktakto/src/App.js
@@ -1,17 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 // Component for thhe board
 // componets for the player icons (x o)
 // start mechanic (begin trackign the game state)
 // know when  3 x's happen or 3 o's happen (flip game state from running to complete, with winner, or draw state)
 
+const emptyBoardState = [
+  ['', '', ''], // row 1 (boardState[0][0] - boardState[0][2])
+  ['', '', ''], // row 3 (boardState[1][0] - boardState[1][2])
+  ['', '', ''], // row 4 (boardState[2][0] - boardState[2][2])
+];
+
 function App() {
+  const [gamePlaying, setGamePlaying] = useState(false); // false will mean there is no game, true will mean there is
+  console.log('gamePlaying: ', gamePlaying);
+  const [winner, setWinner] = useState(null); // false will mean there is no game, true will mean there is
+  const [playerTurn, setPlayerTurn] = useState('x'); // player x will start game, playerTurn will track turn
+  const [boardState, setBoardState] = useState(emptyBoardState);
+
+  const startGame = () => {
+    setGamePlaying(true);
+  };
+
+  // const endGame = () => {
+  //   setGamePlaying(false);
+  // };
+
+  const restartGame = () => {
+    setBoardState(emptyBoardState);
+    setWinner(null);
+  };
+
+  const completeGame = (winner) => {
+    setWinner(winner);
+  };
+
   return (
     <div className="App">
       <Title></Title>
       <GameBoard></GameBoard>
-      <StartButton></StartButton>
+      {<StartButton startGame={startGame}></StartButton>}
     </div>
   );
 }
@@ -69,10 +98,20 @@ function GameBoard() {
   );
 }
 
-function StartButton() {
+function StartButton({ startGame }) {
   return (
-    <button>Start Game</button> // this button should switch to restart or be invisible depending on game state
+    <button onClick={startGame}>Start Game</button> // this button should switch to restart or be invisible depending on game state
   );
+}
+
+// function EndButton({ endGame }) {
+//   return (
+//     <button onClick={endGame}>End Game</button>
+//   );
+// }
+
+function RestartButton({ restartGame }) {
+  return <button onClick={restartGame}>Restart Game</button>;
 }
 
 export default App;
